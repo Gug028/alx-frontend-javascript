eb3c7c26deaf0c846b81e8b7056690e68cb7d553
Gug028 <gramthembu@gmail.com>
2024-06-27 17:31:50 +0200

    import Building from './5-building.js';

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,14 @@
+export default class Building {
+	constructor(sqft) {
+		if (typeof sqft !== 'number') throw new Error('Sqft must be a number');
+		this._sqft = sqft;
+
+		if (this.constructor !== Building && !this.evacuationWarningMessage) {
+			throw TypeError('Class extending Building must override evacuationWarningMessage');
+		}
+	}
+
+	get sqft() {
+		return this._sqft;
+	}
+}
