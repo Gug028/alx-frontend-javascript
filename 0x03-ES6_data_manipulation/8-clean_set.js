4533ac954f58557f652c3ee1a7c6b7d3afa9a3e2
Gug028 <gramthembu@gmail.com>
2024-07-29 22:02:21 +0200

    Clean set

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,5 @@
+export default function cleanSet(set, startString) {
+	  return (startString && startString.length !== 0)
+	    ? [...set].filter((str) => str && str.startsWith(startString))
+	      .map((word) => word.substring(startString.length)).join('-') : '';
+}
